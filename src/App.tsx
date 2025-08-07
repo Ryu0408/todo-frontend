49526e6fb1cd1e11112d00fd2c7e06e4fc3bfba9
Ubuntu <ubuntu@ip-172-31-45-44.ap-northeast-2.compute.internal>
2025-08-07 13:57:56 +0900

    파일 업로드 테스트 및 Jenkins빌드 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploadMessage, setUploadMessage] = useState<string>('');
+
+  // 파일 선택 처리
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  // 파일 업로드 처리
+  const handleFileUpload = async () => {
+    if (!selectedFile) {
+      setUploadMessage('파일을 선택해주세요.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.text();
+      setUploadMessage(data); // 성공 메시지 또는 오류 메시지
+    } catch (error) {
+      setUploadMessage('파일 업로드 실패');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          브랜치 테스트 <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>백엔드 이미지 업로드 테스트</h1>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleFileUpload}>업로드</button>
+        <p>{uploadMessage}</p>
       </header>
     </div>
   );
