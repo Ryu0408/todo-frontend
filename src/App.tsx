96362d38e958ac62b37638b7a5ee4d991b3832b1
Ubuntu <ubuntu@ip-172-31-45-44.ap-northeast-2.compute.internal>
2025-08-18 09:33:24 +0900

    절대경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     formData.append('file', selectedFile);
 
     try {
-      const response = await fetch('https://www.ryu-todo.com/api/upload', {
+      const response = await fetch('https://ryu-todo.com/api/upload', {
         method: 'POST',
         body: formData,
       });
